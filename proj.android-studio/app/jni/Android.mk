32332b6941001109ffa7876cb7d1e9e943f03187
JunHyun Park <junhyunpark@ifunfactory.com>
2017-07-11 16:15:17 +0900

    안드로이드 NDK google test 적용
    프로젝트 설정 변경
    유닛 테스트 코드 파일 추가
    sh 스크립트 추가 (run_funapi_test.sh)
    
    Change-Id: If8740291523e708065ead97160070a338d5ff215

--- proj.android-studio/app/jni/Android.mk
+++ proj.android-studio/app/jni/Android.mk
@@ -40,7 +40,7 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../../Classes/funapi/network/ping_message.pb.cc \
                    ../../../Classes/funapi/service/multicast_message.pb.cc \
                    ../../../Classes/funapi/service/redirect_message.pb.cc \
-                   ../../../Classes/test_messages.pb.cc \
+                   ../../../Classes/test_messages.pb.cc
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
                     $(LOCAL_PATH)/../../../Classes/funapi \
@@ -61,9 +61,26 @@ LOCAL_STATIC_LIBRARIES := cocos2dx_static libsodium libprotobuf cocos_curl_stati
 
 include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := funapi_unittest
+LOCAL_SRC_FILES := hellocpp/unittest.cpp
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
+                    $(LOCAL_PATH)/../../../Classes/funapi \
+                    $(LOCAL_PATH)/../../../Classes/funapi/md5 \
+                    $(LOCAL_PATH)/../../../Classes/funapi/management \
+                    $(LOCAL_PATH)/../../../Classes/funapi/network \
+                    $(LOCAL_PATH)/../../../Classes/funapi/service \
+                    $(LOCAL_PATH)/libprotobuf/include/ARMv7
+LOCAL_SHARED_LIBRARIES := MyGame_shared
+LOCAL_STATIC_LIBRARIES := googletest_main
+LOCAL_CFLAGS += -fPIE
+LOCAL_LDFLAGS += -fPIE -pie
+include $(BUILD_EXECUTABLE)
+
 $(call import-module,.)
 
 # _COCOS_LIB_IMPORT_ANDROID_BEGIN
 # _COCOS_LIB_IMPORT_ANDROID_END
 
 $(call import-module,curl/prebuilt/android)
+$(call import-module,third_party/googletest)
