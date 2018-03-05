682709a1c7f5be1160de80361390d6d268c8b9aa
JunHyun Park <junhyunpark@ifunfactory.com>
2018-03-05 17:40:57 +0900

    압축 기능 추가 - zstd, deflate(zlib)
    const char* 전역 변수를 define 으로 변경
    
    Change-Id: I43e575b4c9bac176a4c7393e44e5abe28f214d45

--- Classes/funapi/funapi_build_config.h
+++ Classes/funapi/funapi_build_config.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2013-2017 iFunFactory Inc. All Rights Reserved.
+// Copyright (C) 2013-2018 iFunFactory Inc. All Rights Reserved.
 //
 // This work is confidential and proprietary to iFunFactory Inc. and
 // must not be used, disclosed, copied, or distributed without the prior
@@ -12,10 +12,13 @@
 #define FUNAPI_API
 #define WITH_FUNAPI 1
 
+#define FUNAPI_HAVE_ZLIB 1
+#define FUNAPI_HAVE_ZSTD 1
 #define FUNAPI_HAVE_DELAYED_ACK 1
 #define FUNAPI_HAVE_TCP_TLS 1
 #define FUNAPI_HAVE_RPC 1
 #define FUNAPI_HAVE_WEBSOCKET 1
+
 #define FUNAPI_HAVE_SODIUM 1
 #define FUNAPI_HAVE_AES128 1
 
