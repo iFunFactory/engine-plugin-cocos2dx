bc43ca0daad902da696ebea22f006f0a598fa947
sungjin.bae <sungjin.bae@ifunfactory.com>
2019-09-23 15:54:46 +0900

    cocos2d-x 3.17.2 업데이트
    3.17.2 업데이트 대응
    - (공통) libwebsockets 2.4.2 업데이트로 프로젝트의 uv 첨조 디렉토리 추가.
    
    ios, mac
    - xcode 11 대응으로 cocos2d 소스인 btVector3.h 의 BT_SHUFFLE define 오류 수정.
      cocos2d-x 에 있는 이슈로 cocos2d-x 버전 업데이트로 fixup 되기 이전까지 위의 수정 적용.
    
    win
    - visual studio 프로젝트의 리소스 디렉토리 위치가 변경되어 Custom Build Setup/build command 옵션도 같이 수정.
    
    android
    - cocos2d-x 프로젝트 생성시 만들어지는 안드로이드 폴더 이름이 변경되어 샘플 프로젝트에도 동일하게 변경.
      android-studio -> android 로 이름이 변경됨.
    
    Change-Id: Ia8854537d6542062907ce7188041a76096892059

--- proj.android-studio/run_funapi_unittest.sh
+++
@@ -1,5 +0,0 @@
-~/Library/Android/sdk/platform-tools/adb push app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/libgnustl_shared.so /data/local/tmp/
-~/Library/Android/sdk/platform-tools/adb push app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/libMyGame.so /data/local/tmp/
-~/Library/Android/sdk/platform-tools/adb push app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/funapi_unittest /data/local/tmp/
-~/Library/Android/sdk/platform-tools/adb shell chmod 755 /data/local/tmp/funapi_unittest
-~/Library/Android/sdk/platform-tools/adb shell "LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/funapi_unittest"
